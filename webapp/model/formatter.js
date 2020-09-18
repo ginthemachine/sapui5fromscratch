79f722d13770f12e9e115383d45d309c6e0bd88f
Gin Rueda <rueda.ginwene@gmail.com>
2020-09-18 15:07:11 +0000

    Added statusIndicator function to formatter.

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -15,6 +15,26 @@ sap.ui.define([
 			default:
 				return ValueState.Information;
 			}
+		},
+
+		statusIndicator: function (sStatus) {
+			var sIcon;
+
+			switch (sStatus) {
+			case "C":
+				sIcon = "completed";
+				break;
+			case "P":
+				sIcon = "in-process";
+				break;
+			case "X":
+				sIcon = "error";
+				break;
+			default:
+				sIcon = "inactive";
+			}
+
+			return "sap-icon://status-" + sIcon;
 		}
 	};
 
